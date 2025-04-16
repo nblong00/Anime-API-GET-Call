f326bc639e82e20ab7ba2fde2713e80cc7afcbaf
Blaise <blazzze0517@gmail.com>
2025-04-15 21:43:08 -0400

    Added try statement to check for errors. Also simiplied containing the JSON response data for easier reading.

--- API-GET-CALL.py
+++ API-GET-CALL.py
@@ -1,5 +1,5 @@
 import requests
-import pandas
+import json
 
 url = 'https://api.jikan.moe/v4/top/anime'
 header={"Content-Type":"application/json",
@@ -10,13 +10,16 @@ print(response)
 
 responseData = response.json()
 
-open('temp.json', 'w').close()
-
 # This can be used to overwrite file that is currently open
 #f = open('file.txt', 'r+')
 #f.truncate(0) # need '0' when using r+
 
-data = pandas.json_normalize(responseData, 'data')
-data.to_json("temp.json")
+try:
+    with open('temp.json', 'w') as file:
+        json.dump(responseData, file, indent=4)  # Formats JSON with indentions
+    print("Data written to temp.json successfully.")
+    input("Operation complete. Please key to close...")
+except IOError as e:
+    print(f"Error writing to file: {e}")
+    input("Operation failed. Please key to close...")
 
-input("Operation complete. Please key to close...")
