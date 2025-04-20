6be986263d6caca18c83faffb469ae71fff73bd7
Blaise <blazzze0517@gmail.com>
2025-04-19 21:22:55 -0400

    Added error checking on user input

--- API-GET-CALL.py
+++ API-GET-CALL.py
@@ -29,8 +29,28 @@ print()
 userInput = input("Enter anime ID you want to query:")
 print()
 
-# User input translated to integer
-mal_id_variable = int(userInput)
+# Error checking for user input
+try:
+    
+    # Convert input to integer
+    mal_id_variable = int(userInput)
+
+    # Check if input is empty
+    if not userInput:
+        raise ValueError("No input provided.")
+    # Confirm if input is positive, else the program should exit
+    if mal_id_variable <= 0:
+        raise ValueError("Anime ID must be a positive integer.")
+
+    # Check if mal_id exists in the JSON data
+    if mal_id_variable not in cleanedUpJSON['mal_id'].values:
+        raise ValueError(f"Anime ID '{mal_id_variable}' not found in the top anime list.")
+
+except ValueError as e:
+    print(f"Error: {e}")
+    input("Operation failed. Press ENTER to close...")
+    exit()
+
 
 # Pull the specific mal_id entry using user input
 filteredMainID = cleanedUpJSON[cleanedUpJSON['mal_id'] == mal_id_variable]
