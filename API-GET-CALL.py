2c78990a7c93b2d9ed2f110fbd887de7903c4bbd
Blaise <blazzze0517@gmail.com>
2025-05-06 22:00:56 -0400

    Encapsulated main API get logic in while statement and added exit logic

--- API-GET-CALL.py
+++ API-GET-CALL.py
@@ -12,66 +12,89 @@ def apiReturnColumns(columns):
     selectedColumns = cleanedUpJSON[desiredColumns]
     return selectedColumns
 
+def exitLogic():
+    for i in range(4):
+        anotherLoop = input("Do you want to run another GET call? (Enter Yes/No) ").title()
+        
+        if anotherLoop == "Yes":
+            break
+        elif i >= 3:
+            return "3"
+        elif anotherLoop == "No":
+            i += 3
+            return anotherLoop
+        elif anotherLoop != "No" or anotherLoop != "Yes":
+            print("Invalid entry.")
 
-# API call occurs & response code is printed
-response = requests.get(url, headers=header)
-print()
-print('Top Anime GET v1.0')
-print()
-time.sleep(1)
+while True:
 
-# Check if API call was successful
-if response.status_code != 200:
-    print(f"Error: API request failed with status code {response.status_code}")
-    input("Press ENTER to close...")
-    exit()
-
-# Call results are stored in variable & json is normalized and cut down to data block
-responseData = response.json()
-cleanedUpJSON = pandas.json_normalize(responseData, 'data')
-
-# Target columns to extract from JSON
-desiredColumns = ['mal_id', 'title_english']  
-print(apiReturnColumns(desiredColumns))
-
-# Error checking for user input
-try:
-# Ask user what top anime they'd like to read more about (ID)
+    # API call occurs & response code is printed
+    response = requests.get(url, headers=header)
     print()
-    mal_id_variable = input("Enter anime ID you want to query: ")
+    print('Top Anime GET v1.0')
     print()
+    time.sleep(1)
 
-    # Check if input is empty
-    if not mal_id_variable:
-        raise ValueError("No input provided.")
+    # Check if API call was successful
+    if response.status_code != 200:
+        print(f"Error: API request failed with status code {response.status_code}")
+        input("Press ENTER to close...")
+        exit()
+
+    # Call results are stored in variable & json is normalized and cut down to data block
+    responseData = response.json()
+    cleanedUpJSON = pandas.json_normalize(responseData, 'data')
+
+    # Target columns to extract from JSON
+    desiredColumns = ['mal_id', 'title_english']  
+    print(apiReturnColumns(desiredColumns))
+
+    # Error checking for user input
+    try:
+    # Ask user what top anime they'd like to read more about (ID)
+        print()
+        mal_id_variable = input("Enter anime ID you want to query: ")
+        print()
+
+        # Check if input is empty
+        if not mal_id_variable:
+            raise ValueError("No input provided.")
+        
+        # Confirm if input is positive, else the program should exit
+        elif int(mal_id_variable) <= 0:
+            raise ValueError("Anime ID must be a positive integer.")
+
+        # Check if mal_id exists in the JSON data
+        elif int(mal_id_variable) not in cleanedUpJSON['mal_id'].values:
+            raise ValueError(f"Anime ID '{mal_id_variable}' not found in the top anime list.")
+
+        # Switch user input to integer after error checking
+        else:
+            mal_id_variable = int(mal_id_variable)
+
+    except ValueError as e:
+        print(f"Error: {e}")
+        input("Operation failed. Press ENTER to close...")
+        exit()
+
+    # Pull the specific mal_id entry using user input
+    cleanedUpJSON = cleanedUpJSON[cleanedUpJSON['mal_id'] == mal_id_variable]
+
+    # Target columns to extract from mal_id entered by user (reuse desired_columns & selected columns)
+    desiredColumns = ['mal_id', 'url', 'title_english']  
+    print(apiReturnColumns(desiredColumns))
+
+    time.sleep(.5)
+    print()
     
-    # Confirm if input is positive, else the program should exit
-    elif int(mal_id_variable) <= 0:
-        raise ValueError("Anime ID must be a positive integer.")
+    exitChoice = exitLogic()
 
-    # Check if mal_id exists in the JSON data
-    elif int(mal_id_variable) not in cleanedUpJSON['mal_id'].values:
-        raise ValueError(f"Anime ID '{mal_id_variable}' not found in the top anime list.")
-
-    # Switch user input to integer after error checking
-    else:
-        mal_id_variable = int(mal_id_variable)
-
-except ValueError as e:
-    print(f"Error: {e}")
-    input("Operation failed. Press ENTER to close...")
-    exit()
-
-# Pull the specific mal_id entry using user input
-cleanedUpJSON = cleanedUpJSON[cleanedUpJSON['mal_id'] == mal_id_variable]
-
-# Target columns to extract from mal_id entered by user (reuse desired_columns & selected columns)
-desiredColumns = ['mal_id', 'url', 'title_english']  
-print(apiReturnColumns(desiredColumns))
+    if exitChoice == "No" or exitChoice == "3":
+        break
 
 # Wait for user input to close window 
 print()
-input("Operation complete. Please ENTER to close...")
+input("Program exiting. Please ENTER to close...")
 
 # Leaving in to let me get copy of the fields returned
 # cleanedUpJSON.to_csv('E:\\AWS_DEV\\PythonScripts\\API-GET-CALL\\temp.csv')
